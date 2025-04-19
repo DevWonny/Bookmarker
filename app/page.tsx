b84a95a6f9a4dbfdd00b14a6ca80963cb3663c39
DevWonny <cjfdnjs1994@naver.com>
2025-04-19 23:25:41 +0900

    Main Rank List Filter Click Event Add

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,32 @@ import ListItem from "@/components/common/listItem";
 import "@/styles/pages/main.scss";
 
 export default () => {
+  const [listFilter, setListFilter] = useState("week");
+
   return (
     <div className="main-wrap w-full">
       <div className="banner-container w-full">Banner Container</div>
 
       <div className="rank-list-container w-full flex flex-col">
         <div className="btn-container flex justify-end">
-          <button className="week-btn">주간</button>
-          <button className="month-btn">월간</button>
-          <button className="year-btn">연간</button>
+          <button
+            className={`week-btn ${listFilter === "week" && "active"}`}
+            onClick={() => setListFilter("week")}
+          >
+            주간
+          </button>
+          <button
+            className={`month-btn ${listFilter === "month" && "active"}`}
+            onClick={() => setListFilter("month")}
+          >
+            월간
+          </button>
+          <button
+            className={`year-btn ${listFilter === "year" && "active"}`}
+            onClick={() => setListFilter("year")}
+          >
+            연간
+          </button>
         </div>
 
         <div className="list-item-container">
