955ad841f31b070e3fe15d94a58f50dbee622178
DevWonny <cjfdnjs1994@naver.com>
2025-04-19 22:30:51 +0900

    Main Banner 영역 위치 잡기 - Swiper 연결 아직 안함

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+import { useState } from "react";
+// style
+import "@/styles/pages/main.scss";
+
 export default () => {
   return (
-    <div>
-      <div>Main 영역</div>
+    <div className="main-wrap w-full">
+      <div className="banner-container w-full">Banner Container</div>
     </div>
   );
 };
