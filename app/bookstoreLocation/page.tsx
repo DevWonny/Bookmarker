13ffa0b5c5991331e81414f1dccc333d962a0380
DevWonny <cjfdnjs1994@naver.com>
2025-04-22 12:30:16 +0900

    Location Page 구역 지정

--- app/bookstoreLocation/page.tsx
+++ app/bookstoreLocation/page.tsx
@@ -1,4 +1,14 @@
 // 서점 위치 페이지
+// style
+import "@/styles/pages/bookstoreLocation.scss";
 export default () => {
-  return <div>서점 위치 페이지</div>;
+  return (
+    <div className="store-location-wrap flex w-100">
+      <div className="address-container">Address</div>
+
+      <div className="map-container">Map</div>
+
+      <div className="location-list-container">List</div>
+    </div>
+  );
 };
