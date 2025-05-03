628be1a7a3ae653f23771416f0c46ac8099b874c
DevWonny <cjfdnjs1994@naver.com>
2025-05-03 18:41:15 +0900

    Route 변경 시 Modal 꺼지도록 구현

--- app/layoutClient.tsx
+++ app/layoutClient.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
 
 // component
 import Header from "@/components/common/header";
@@ -11,11 +12,16 @@ export default function LayoutClient({
   // state
   const [isLoginClick, setIsLoginClick] = useState(false);
   const [isSignupClick, setIsSignupClick] = useState(false);
+  const pathname = usePathname();
 
   const onModalClose = () => {
     setIsLoginClick(false);
     setIsSignupClick(false);
   };
+
+  useEffect(() => {
+    onModalClose();
+  }, [pathname]);
 
   return (
     <div>
