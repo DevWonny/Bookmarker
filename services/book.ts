43424cac48d367c9ab39ee965de4a5f095a847db
DevWonny <cjfdnjs1994@naver.com>
2025-05-12 10:12:02 +0900

    알라딘 API에서 description이 없는 아이템 제거

--- services/book.ts
+++ services/book.ts
@@ -16,7 +16,7 @@ export const BookList = async (type: string) => {
     // * Main Banner 사용 용도
     if (type === 'ItemNewSpecial') {
       const res = await axios.get('/api/aladin/bookList', {
-        params: { ttbkey: key, queryType: type, searchTarget: 'Book', Version: 20131101, maxResults: 3, Cover: 'Big' }
+        params: { ttbkey: key, queryType: type, searchTarget: 'Book', Version: 20131101, maxResults: 10, Cover: 'Big' }
       });
 
       const { data, status } = res;
@@ -24,7 +24,7 @@ export const BookList = async (type: string) => {
         return 'Book List Main Banner Error';
       }
 
-      return data.item;
+      return data.item.filter((el: any) => el.description).slice(0, 3);
     }
 
     // * Main 하단 Bestseller List
