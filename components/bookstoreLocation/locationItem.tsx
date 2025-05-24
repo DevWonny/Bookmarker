5cafc173ee7a255e74145a2211708038728faf61
DevWonny <cjfdnjs1994@naver.com>
2025-05-24 21:42:34 +0900

    Location List Icon 제거

--- components/bookstoreLocation/locationItem.tsx
+++ components/bookstoreLocation/locationItem.tsx
@@ -35,16 +35,10 @@ export default function LocationItem({
       onMouseOver={() => onOver(item.id)}
       onMouseLeave={() => onLeave(item.id)}
     >
-      <div className="title-container flex items-center">
-        <div className="icon"></div>
-        <div className="title">{item.place_name}</div>
-      </div>
-
-      <div className="detail-container flex flex-col ">
-        <p className="address">{item.road_address_name}</p>
-        <p className="phone-number">{item.phone}</p>
-        <p className="distance">{distanceConvert()}</p>
-      </div>
+      <div className="title">{item.place_name}</div>
+      <p className="address">{item.road_address_name}</p>
+      <p className="phone-number">{item.phone}</p>
+      <p className="distance">{distanceConvert()}</p>
     </div>
   );
 }
