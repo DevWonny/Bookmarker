e821ebf8bf05ab9d77be202e4021bd2e8cb8ffa4
DevWonny <cjfdnjs1994@naver.com>
2025-06-04 15:16:03 +0900

    Wish List 추가 및 제거, 조회 기능 구현진행중(supabase)

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -1,9 +1,26 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+// store
+import { useAuth } from "@/stores/auth";
+// service
+import {
+  fetchWishList,
+  addWishItem,
+  removeWishItem,
+} from "@/services/wishlist";
 // component
 import ListItem from "@/components/common/listItem";
 
 export default () => {
+  const [userId, setUserId] = useState(null);
+  const { session } = useAuth();
+
+  useEffect(() => {
+    if (session && session.user) {
+      setUserId(session.user.id);
+    }
+  }, [session]);
+
   return (
     <div className="wishlist-warp">
       WishList
---
+++ services/wishlist.ts
@@ -0,0 +1,41 @@
+import { supabase } from "@/lib/supabase";
+
+// * fetch Wish List
+export const fetchWishList = async (userId: string) => {
+  const res = await supabase.from('wishlist').select('item_id').eq('user_id', userId);
+  const { error, data } = res;
+  if (error) {
+    console.log('Fetch Wish List Error - ', error);
+    return;
+  }
+
+  console.log("🚀 ~ fetchWishList ~ data:", data)
+  return data;
+}
+
+// * add Wish Item
+export const addWishItem = async (userId: string, itemId: string) => {
+  // ! 추가 시 데이저 정리 해서 추가  
+
+  const req = await supabase.from('wishlist').insert({ user_id: userId, item_id: itemId });
+  const { error, data } = req;
+
+  if (error) {
+    console.log('Add Wish Item Error - ', error);
+    return;
+  }
+
+  console.log("🚀 ~ addWishItem ~ data:", data)
+  return data;
+}
+
+// * Remove Wish Item
+export const removeWishItem = async (userId: string, itemId: string) => {
+  const req = await supabase.from('wishlist').delete().match({ user_id: userId, item_id: itemId });
+  const { error, data } = req;
+  if (error) {
+    console.log('Remove Wish Item Error - ', error)
+    return;
+  }
+  console.log("🚀 ~ removeWishItem ~ data :", data)
+}
