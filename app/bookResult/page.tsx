c0a9c0d3bd7a38214797426b142eee732a1bfc1d
DevWonny <cjfdnjs1994@naver.com>
2026-02-13 01:13:17 +0900

    검색 결과 없을 경우 아이콘 추가

--- app/bookResult/page.tsx
+++ app/bookResult/page.tsx
@@ -10,6 +10,7 @@ import ListItem from "@/components/common/listItem";
 import { BookItem } from "@/types/main";
 // style
 import "@/styles/pages/bookResult.scss";
+import { BookAlert } from "lucide-react";
 
 export default () => {
   // * 검색 결과 데이터가 있을 경우에는 리스트를, 없을 경우에는 문구 표출
@@ -47,7 +48,9 @@ export default () => {
 
       {!isData && (
         <div className="warning-box flex flex-col items-center">
-          <div className="warning-icon"></div>
+          <div className="warning-icon">
+            <BookAlert className="icon" />
+          </div>
           <p className="warning-text">
             {`검색 결과가 없습니다.\n검색어의 철자가 정확한지 다시 한 번 확인해주세요.`}
           </p>
